aadcd599f9af497bfb3a2bc4d03780ea786538f7
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-08-16 21:00:22 +0300

    Aquarium structure 50/50

---
+++ aquarium/core/ControllerImpl.java
@@ -0,0 +1,5 @@
+package aquarium.core;
+
+public class ControllerImpl {
+
+}
---
+++ aquarium/entities/aquariums/BaseAquarium.java
@@ -0,0 +1,83 @@
+package aquarium.entities.aquariums;
+
+import aquarium.entities.decorations.Decoration;
+import aquarium.entities.fish.Fish;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static aquarium.common.ConstantMessages.NOT_ENOUGH_CAPACITY;
+import static aquarium.common.ExceptionMessages.AQUARIUM_NAME_NULL_OR_EMPTY;
+
+public abstract class BaseAquarium implements Aquarium{
+
+    private String name;
+    private int capacity;
+    private Collection<Decoration> decorations;
+    private Collection<Fish> fish;
+
+    public BaseAquarium(String name, int capacity) {
+        setName(name);
+        this.capacity = capacity;
+        this.decorations = new ArrayList<>();
+        this.fish = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(AQUARIUM_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public int calculateComfort() {
+        return decorations.stream().mapToInt(Decoration::getComfort).sum();
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void addFish(Fish fish) {
+        if (this.fish.size() == capacity) {
+            throw new IllegalStateException(NOT_ENOUGH_CAPACITY);
+        }
+    }
+
+    @Override
+    public void removeFish(Fish fish) {
+        this.fish.remove(fish);
+    }
+
+    @Override
+    public void addDecoration(Decoration decoration) {
+        this.decorations.add(decoration);
+    }
+
+    @Override
+    public void feed() {
+        fish.forEach(Fish::eat);
+    }
+
+    @Override
+    public String getInfo() {
+        return String.format("%s (%s):", name, getClass().getSimpleName()) + System.lineSeparator()
+                +"Fish: " + ((this.fish.size() == 0) ? "none" : fish.stream().map(Fish::getName).collect(Collectors.joining(" ")))
+                + System.lineSeparator() + "Decorations: " + decorations.size() + System.lineSeparator()
+                + "Comfort: " + calculateComfort();
+    }
+
+    @Override
+    public Collection<Fish> getFish() {
+        return fish;
+    }
+
+    @Override
+    public Collection<Decoration> getDecorations() {
+        return decorations;
+    }
+}
